e3a91a399f29dcadc157b461e7c2fbb382ce6012
FarouQQQ <omarf.essam@gmail.com>
2021-06-13 16:45:36 +0200

    Add repulsion and tuned gains for apf

--- APF.py
+++ APF.py
@@ -31,13 +31,17 @@ import math
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 
 #Global Variables:
-#Gains:
-KP = 0.1 #Position Gain
+#Atracton Gains:
+KP = 1 #Position Gain
+
+#repulsion gains:
+eta = 25
+rho_node = 100
 
 #Map specific:
 mapBoundaryX = 10   # Map x length
 mapBoundaryY = 10   # Map y length
-reso = 5    #Map resolution
+reso = 2    #Map resolution
 
 spacingResolution = (mapBoundaryX * reso) + 1
 
@@ -45,17 +49,23 @@ xAxis = np.linspace(0, mapBoundaryX, spacingResolution)
 yAxis = np.linspace(0, mapBoundaryY, spacingResolution)
 
 #Robot specific
-robotX = 0  #initial robot X position
-robotY = 0  #initial robot Y position
+robotX = 0.0  #initial robot X position
+robotY = 0.0  #initial robot Y position
 
-goalX = 5   #goal X position
-goalY = 5   #goal Y position
+goalX = 10.0   #goal X position
+goalY = 10.0   #goal Y position
 
-# xGoalOnGrid = (goalX/mapBoundaryX) * spacingResolution
-# yGoalOnGrid = (goalY/mapBoundaryY) * spacingResolution
+#Obstical:
+obsX = 5.0
+obsY = 5.0
+
+#Position scaling for grid:
 
 xGoalOnGrid = goalX * reso
 yGoalOnGrid = goalY * reso
+
+xObsOnGrid = obsX * reso
+yObsOnGrid = obsY * reso
 
 #potential field generation:
 uPot = np.zeros(shape=(len(xAxis),len(yAxis)))
@@ -65,18 +75,31 @@ uRep = np.zeros(shape=(len(xAxis),len(yAxis)))
 #Calculate Attraction
 for i in range(len(xAxis)):
     for j in range(len(yAxis)):
-        uAtr[i][j] = 1 / 2 * KP * math.sqrt(((xGoalOnGrid-(i))**2+(yGoalOnGrid - (j))**2))
+        uAtr[i][j] = 1 / 2.0 * KP * math.sqrt(abs(xGoalOnGrid-(i))**2+abs(yGoalOnGrid - (j))**2)
 
+
+#Calculate repultion potential:
+for i in range(len(xAxis)):
+    for j in range(len(yAxis)):
+        rho =  math.sqrt(((xObsOnGrid-(i))**2+(yObsOnGrid - (j))**2))
+        if(rho <= rho_node):
+            if(rho==0):
+                uRep[i][j] = np.max(uRep)
+            else :
+                uRep[i][j] = (1/2) * eta * ((1/rho)-(1/rho_node))**2
+        else:
+            uRep[i][j] = 0
 
 uPot = uAtr + uRep
 
-print(uPot.shape)
-print(uPot)
+# for debugging
+# print(uRep.shape)
+# print(uRep)
 
 xAxis, yAxis = np.meshgrid(xAxis, yAxis)
 
 surf = ax.plot_surface(xAxis, yAxis, uPot, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
+                       linewidth=0, antialiased=True)
 
 
 # for i in range(5):
